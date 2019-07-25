e0d24194c55e8402ed5f6dd20ee1eaba2eccf94e
Erick Marín <erick.marin.v@gmail.com>
2019-07-25 16:52:52 -0600

    IndexedDB

--- UnidadI/registro-facebook-indexeddb/public/js/controlador.js
+++ UnidadI/registro-facebook-indexeddb/public/js/controlador.js
@@ -1,5 +1,47 @@
-var localStorage = window.localStorage;
-llenarTabla();
+/*
+Modelo: Logica de negocio y los datos (BD, Backend)
+Vista: Lo que el usuario ve UI (HTML, CSS)
+Controlador: Controlar y responder las acciones del usuario (Javascript)
+
+Angular, VueJS, React
+NodeJS, Express, Gestor de vistas EJS, Pug(Jade)
+*/
+var db;
+
+(()=>{
+    if (!('indexedDB' in window)) 
+        console.log('Este navegador no soporta indexedDB');
+    
+        //Si la base de datos no existe la crea, sino solo la abre.
+        let solicitud = window.indexedDB.open('facebook',1);  //Asincrona
+        
+        solicitud.onsuccess = function(evento){
+            console.log("Se creó o abrió la BD");
+            db = solicitud.result;
+            llenarTabla();
+        }
+
+        solicitud.onerror = function(evento){
+            console.log(evento);
+        }
+
+        //Se ejecuta cuando se crea o se necesita actualizar la BD
+        solicitud.onupgradeneeded = function(evento){
+            //En este punto si se podria crear las colecciones
+            console.log('Se creo o actualizó la BD');
+            let db = evento.target.result;
+            //Las colecciones en IndexedDB se les llama ObjectStores
+            let objectStoreUsuarios = db.createObjectStore('usuarios',{keyPath:'codigo', autoIncrement:true});
+            objectStoreUsuarios.transaction.oncomplete = function(evento){
+                console.log('Se creo el object store de usuarios');
+            }
+
+            objectStoreUsuarios.transaction.onerror = function(evento){
+                console.log(evento);
+            }
+        }
+        
+})();
 
 var campos = [
     {id:'first-name', valido:false},
@@ -10,8 +52,6 @@ var campos = [
     {id:'day', valido:false},
     {id:'year', valido:false}
 ];
-
-var registros = []; //global
 
 let months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
 
@@ -50,16 +90,21 @@ function registrarUsuario(){
             year: document.getElementById('year').value
         }
     }
-    registros.push(persona);
-    //--------------
-    let key = localStorage.key(localStorage.length) + 1;
-    localStorage.setItem(key,JSON.stringify(persona));
     
-    //---------------
+    //Agregar al ObjectStore de usuarios
+    let transaccion = db.transaction(['usuarios'],'readwrite'); //readonly: Solo lectura, readwrite:lectura y escritura
+    let objectStoreUsuarios = transaccion.objectStore('usuarios');
+    let solicitud = objectStoreUsuarios.add(persona);
+    solicitud.onsuccess = function(evento){
+        console.log('Se agrego el registro con exito');
+        console.log(evento);
+        anexarFilaTabla(persona, evento.target.result);
+        document.getElementById('resultado').style.display = "block";
+    }
 
-    anexarFilaTabla(persona, key);
-    document.getElementById('resultado').style.display = "block";
-    console.log(registros);
+    solicitud.onerror = function(evento){
+        console.log(evento);
+    }
 }
 
 
@@ -91,14 +136,21 @@ function marcarInput(id, valido){
 
 
 function llenarTabla(){
-    document.getElementById('tabla-registros').innerHTML = "";
-    for (let i=0; i<localStorage.length;i++){
-        let key = localStorage.key(i);
-        anexarFilaTabla(JSON.parse(localStorage.getItem(key)),key);
+    document.getElementById('tabla-registros').innerHTML = '';
+    let transaccion = db.transaction(['usuarios'],'readonly');
+    let objectStoreUsuarios = transaccion.objectStore('usuarios');
+    let cursor = objectStoreUsuarios.openCursor();
+    cursor.onsuccess = function(evento){
+        //Se ejecuta por cada registro en el objectstore
+        if (evento.target.result){
+            console.log(evento.target.result);
+            anexarFilaTabla(evento.target.result.value, evento.target.result.key);
+            evento.target.result.continue(); //Mover el cursor a la siguiente direccion de memoria
+        }
     }
 }
 
-function anexarFilaTabla(persona, key){
+function anexarFilaTabla(persona, keyPath){
     document.getElementById('tabla-registros').innerHTML += 
                     `<tr>
                         <td>${persona.firstName}</td>
@@ -107,18 +159,27 @@ function anexarFilaTabla(persona, key){
                         <td>${persona.gender}</td>
                         <td>${persona.password}</td>
                         <td>${persona.birthdate.day}/${persona.birthdate.month}/${persona.birthdate.year}</td>
-                        <td><button type="button" onclick="eliminar(${key})"><i class="fas fa-trash-alt"></i></button></td>
-                        <td><button type="button" onclick="editar(${key})"><i class="fas fa-edit"></i></button></td>
+                        <td><button type="button" onclick="eliminar(${keyPath})"><i class="fas fa-trash-alt"></i></button></td>
+                        <td><button type="button" onclick="editar(${keyPath})"><i class="fas fa-edit"></i></button></td>
                     </tr>`;
 }
 
 function eliminar(key){
-    console.log("Eliminar registro "+key);
-    localStorage.removeItem(key);
-    llenarTabla();
+    let transaccion = db.transaction(['usuarios'],'readwrite');
+    let objectStoreUsuarios = transaccion.objectStore('usuarios');
+    let solicitud = objectStoreUsuarios.delete(key);
+    solicitud.onsuccess = function(){
+        console.log('Se elimino el registro');
+        llenarTabla();
+    }    
 }
 
 function editar(key){
     console.log("Editar registro "+key+", en este caso deberia obtener el JSON del LocalStorage y de sus valores llenar los input del formulario, cambiar el boton por uno que diga actualizar y sustituir el json del LocalStorage por la informacion actualizada");
-
+    let transaccion = db.transaction(['usuarios'],'readonly');
+    let objectStoreUsuarios = transaccion.objectStore('usuarios');
+    let solicitud = objectStoreUsuarios.get(key);
+    solicitud.onsuccess = function(evento){
+        console.log(evento.target.result);
+    }
 }
